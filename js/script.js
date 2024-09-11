233556cec5334c7e2fa7e52e0ca310a18aa720dd
gumarshams <gumarshams>
2024-09-11 15:25:51 +0300

    first coomit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use srtict" 
+"use srtict";
  
 //1 
  
@@ -16,48 +16,48 @@ if (a > 0 && b> 0){
 } 
 //  2 задание
 
-// let a = prompt("введите число", 10);
-// if(a > 0){
-//     alert(1);
-// }
-// else if(a < 0){
-//     alert(-1);
-// }
-// else if(a == 0){
-//     alert(0);
-// }
+let c = prompt("введите число", 10);
+if(c > 0){
+    alert(1);
+}
+else if(c < 0){
+    alert(-1);
+}
+else if(c == 0){
+    alert(0);
+}
 
 // 3 задание
 
-// let result;
-// let a = Number(prompt('pr1', 0));
-// let b = Number(prompt('pr2', 0));
-// (a + b < 4) ? result = 'Мало' : result = 'Много';
-// alert(result);
+let result;
+let pr1 = Number(prompt('pr1', 0));
+let pr2 = Number(prompt('pr2', 0));
+(pr1 + pr2 < 4) ? result = 'Мало' : result = 'Много';
+alert(result);
 
 // 4 задание
-// let message;
-// let login = prompt('вы кто', '');
-// (login == 'Сотрудник') ? message = 'Привет' :
-// (login == 'Директор') ? message = 'Здравствуйте' :
-// (login == '') ? message = 'Нет логина' :
-//   message = '';
+let message;
+let login = prompt('вы кто', '');
+(login == 'Сотрудник') ? message = 'Привет' :
+(login == 'Директор') ? message = 'Здравствуйте' :
+(login == '') ? message = 'Нет логина' :
+  message = '';
 
 // 5 задание
 
-// let login = prompt('Напишите свой логин', '');
-// if(login == 'Админ' || login == 'админ'){
-//   let pass = prompt('Напишите пароль', '');
-//   if(pass == 'Я главный'){
-//     alert('Здравствуйте');
-//   }
-//   else if (pass == null){
-//     alert('Отменено');
-//   }
-//   else if (pass != 'Я главный'){
-//     alert('неправильный пароль');
-//   }
-// }
-// else {
-//   alert('я не знаю');
-// }
+let log = prompt('Напишите свой логин', '');
+if(log == 'Админ' || log == 'админ'){
+  let pass = prompt('Напишите пароль', '');
+  if(pass == 'Я главный'){
+    alert('Здравствуйте');
+  }
+  else if (pass == null){
+    alert('Отменено');
+  }
+  else if (pass != 'Я главный'){
+    alert('неправильный пароль');
+  }
+}
+else {
+  alert('я не знаю');
+}
